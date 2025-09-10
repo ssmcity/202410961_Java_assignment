25073dbdc6af506ad9508355bc21be188ce60fdb
ssmcity <ssmcitycjycity@gmail.com>
2025-09-10 19:05:17 +0900

    Add files via upload

---
+++ thisisjava/src/ch01/sec10/Calculator.java
@@ -0,0 +1,14 @@
+package ch01.sec10;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1;
+		int y=2;
+		int result = x+y;
+		System.out.println(result);
+
+	}
+
+}
